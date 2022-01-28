d6d38ff1b07866c603a1f58366fc94604e603807
Anthony <24651774+anthonyaquino83@users.noreply.github.com>
2022-01-28 13:54:37 -0300

    remove equatable

--- lib/providers/notes_state.dart
+++ lib/providers/notes_state.dart
@@ -1,22 +1,15 @@
-import 'package:equatable/equatable.dart';
 import 'package:riverpodcrud/models/note.dart';
 
-abstract class NotesState extends Equatable {
+abstract class NotesState {
   NotesState();
 }
 
 class NotesInitial extends NotesState {
   NotesInitial();
-
-  @override
-  List<Object?> get props => [];
 }
 
 class NotesLoading extends NotesState {
   NotesLoading();
-
-  @override
-  List<Object?> get props => [];
 }
 
 class NotesLoaded extends NotesState {
@@ -25,23 +18,14 @@ class NotesLoaded extends NotesState {
   });
 
   final List<Note>? notes;
-
-  @override
-  List<Object?> get props => [notes];
 }
 
 class NotesEmpty extends NotesState {
   NotesEmpty();
-
-  @override
-  List<Object?> get props => [];
 }
 
 class NotesFailure extends NotesState {
   NotesFailure();
-
-  @override
-  List<Object?> get props => [];
 }
 
 class NotesValidating extends NotesState {
@@ -54,9 +38,6 @@ class NotesValidating extends NotesState {
   final Note? note;
   final String? tituloMessage;
   final String? conteudoMessage;
-
-  @override
-  List<Object?> get props => [tituloMessage, conteudoMessage];
 }
 
 class NotesValidated extends NotesState {
@@ -65,14 +46,8 @@ class NotesValidated extends NotesState {
   });
 
   final Note? note;
-
-  @override
-  List<Object?> get props => [note];
 }
 
 class NotesSuccess extends NotesState {
   NotesSuccess();
-
-  @override
-  List<Object?> get props => [];
 }
